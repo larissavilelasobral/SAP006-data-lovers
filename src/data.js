07e2f6b3a6a99eff2844f480cd93fee3ea31cae0
Larissa Siqueira <larissaraie@gmail.com>
2021-07-06 23:39:37 -0300

    tá tudo funcionando, não aguento mais

--- src/data.js
+++ src/data.js
@@ -1,34 +1,34 @@
 const allFilters = {
-searchCharacter: function searchCharacter(data, input){
-  const searchChampion = data.filter(item => item.name.toLowerCase().includes(input))
-  return searchChampion
-},
-orderAZ: function orderAZ(data){
-  const resultsAZ = data.sort((a, z) => a.name > z.name ? 1 : -1)
-  return resultsAZ
-},
-orderZA: function orderZA(data){
-  const resultsZA = data.sort((a, z) => a.name < z.name ? 1 : -1)
-  return resultsZA
-},
-filterTag: function filterTag(data,tag){
-  const charactersTags = data.filter(function(item){
-  return item.tags.includes(tag);
-  })
-  return charactersTags
-},
-filterDifficultyMenor: function filterDifficulty(data, difficulty){
-  const charactersFilter = data.filter(function(champion){
-  return champion.info.difficulty <= difficulty;
-  })
-  return charactersFilter
-},
-filterDifficultyMaior: function filterDifficulty(data, difficulty){
-  const charactersFilter = data.filter(function(champion){
-  return champion.info.difficulty >= difficulty;
-  })
-  return charactersFilter
-}
-}
-
-export default allFilters
+  searchCharacter: function (data, input){
+    const searchChampion = data.filter(item => item.name.toLowerCase().includes(input))
+    return searchChampion
+  },
+  orderAZ: function (data){
+    const resultsAZ = data.sort((a, z) => a.name > z.name ? 1 : -1)
+    return resultsAZ
+  },
+  orderZA: function (data){
+    const resultsZA = data.sort((a, z) => a.name < z.name ? 1 : -1)
+    return resultsZA
+  },
+  filterTag: function (data,tag){
+    const charactersTags = data.filter(function(item){
+     return item.tags.includes(tag);
+    })
+    return charactersTags
+  },
+    filterDifficultyEasy: function (data, difficulty){
+      const charactersFilter = data.filter(function(champion){
+      return champion.info.difficulty <= difficulty;
+      })
+      return charactersFilter
+    },
+  filterDifficultyHard: function (data, difficulty){
+    const charactersFilter = data.filter(function(champion){
+    return champion.info.difficulty >= difficulty;
+    })
+    return charactersFilter
+  }
+  }
+  
+  export default allFilters
