4393085e8545a44ab29078c3509a4a32954b8b98
Larissa Siqueira <larissaraie@gmail.com>
2021-07-10 03:34:53 -0300

    mudei o menu da página com uma seta pra baixo e os testes estão cobrindo mais de 70

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,11 @@
 import allFilters from '../src/data.js';
 
+const data = [
+  {"name": "Aatrox", "key": "266", "info": {"difficulty": "4"}, "tags": ["Fighter", "Tank"]},
+  {"name": "Ahri", "key": "103", "info": {"difficulty": "5"}, "tags": ["Mage", "Assassin"]},
+  {"name": "Ashe", "key": "22", "info": {"difficulty": "4"}, "tags": ["Marksman", "Support"]},
+  {"name": "Zed", "key": "238", "info": {"difficulty": "7"}, "tags": ["Assassin", "Fighter"]}
+]
 
 describe('allFilters', () => {
 
@@ -8,10 +14,22 @@ describe('allFilters', () => {
 })
 });
 
+const procurarPersonagens = [
+  {"name": "Aatrox"},
+  {"name": "Ziggs"}
+]
 describe('searchCharacter', () => {
-
   it('should be a function', () => {
     expect(typeof allFilters.searchCharacter).toBe('function');
+  })
+  it('filtro de pesquisa', () => {
+    const personagemA = [{"name": "Aatrox"}]
+    expect(allFilters.searchCharacter(procurarPersonagens, "a")).toEqual(personagemA)
+  })
+  it('should throw TypeError when invoked with wrong argument types', () => {
+    expect(() => allFilters.orderAZ("")).toThrow(TypeError);
+    expect(() => allFilters.orderAZ(0)).toThrow(TypeError);
+    expect(() => allFilters.orderAZ(null)).toThrow(TypeError);
   })
   });
 
@@ -19,43 +37,114 @@ describe('orderAZ', () => {
   it('should be a function', () => {
     expect(typeof allFilters.orderAZ).toBe('function');
   })
-  // it('should return "" for ""', () => {
-  //   expect(allFilters.orderAZ('')).toBe('');
-  // })
+  it("filtrar personagens de A a Z", () => {
+    const ordemAZ = [
+      {"name": "Aatrox", "key": "266", "info": {"difficulty": "4"}, "tags": ["Fighter", "Tank"]},
+      {"name": "Ahri", "key": "103", "info": {"difficulty": "5"}, "tags": ["Mage", "Assassin"]},
+      {"name": "Ashe", "key": "22", "info": {"difficulty": "4"}, "tags": ["Marksman", "Support"]},
+      {"name": "Zed", "key": "238", "info": {"difficulty": "7"}, "tags": ["Assassin", "Fighter"]}
+    ]
+    expect(allFilters.orderAZ(data)).toEqual(ordemAZ);
+  })
+  it('should throw TypeError when invoked with wrong argument types', () => {
+    expect(() => allFilters.orderAZ("")).toThrow(TypeError);
+    expect(() => allFilters.orderAZ(0)).toThrow(TypeError);
+    expect(() => allFilters.orderAZ(null)).toThrow(TypeError);
+  })
  });
 
  describe('orderZA', () => {
   it('should be a function', () => {
-    expect(typeof allFilters.orderZA).toBe('function');
+    expect(typeof allFilters.orderAZ).toBe('function');
   })
-//   it('should return "" for ""', () => {
-//     expect(allFilters.orderZA()).toBe();
-//   })
+  it("filtrar personagens de Z a A", () => {
+    const ordemZA = [
+      {"name": "Zed", "key": "238", "info": {"difficulty": "7"}, "tags": ["Assassin", "Fighter"]},
+      {"name": "Ashe", "key": "22", "info": {"difficulty": "4"}, "tags": ["Marksman", "Support"]},
+      {"name": "Ahri", "key": "103", "info": {"difficulty": "5"}, "tags": ["Mage", "Assassin"]},
+      {"name": "Aatrox", "key": "266", "info": {"difficulty": "4"}, "tags": ["Fighter", "Tank"]}
+    ]
+    expect(allFilters.orderZA(data)).toEqual(ordemZA);
+  })
+  it('should throw TypeError when invoked with wrong argument types', () => {
+    expect(() => allFilters.orderAZ("")).toThrow(TypeError);
+    expect(() => allFilters.orderAZ(0)).toThrow(TypeError);
+    expect(() => allFilters.orderAZ(null)).toThrow(TypeError);
+  })
  });
 
  describe('filterTag', () => {
   it('should be a function', () => {
     expect(typeof allFilters.filterTag).toBe('function');
   })
-  // it('should return "" for "" with tag', () => {
-  //   expect(allFilters.filterTag("")).toBe("");
-  // })
+  it('filtrar categoria Tank', () => {
+    const tanks = [
+      {"name": "Aatrox", "key": "266", "info": {"difficulty": "4"}, "tags": ["Fighter", "Tank"]},
+    ]
+    expect(allFilters.filterTag(data, "Tank")).toEqual(tanks);
+  })
+  it('filtrar categoria Fighter', () => {
+    const fighters = [
+      {"name": "Zed", "key": "238", "info": {"difficulty": "7"}, "tags": ["Assassin", "Fighter"]},
+      {"name": "Aatrox", "key": "266", "info": {"difficulty": "4"}, "tags": ["Fighter", "Tank"]}
+    ]
+    expect(allFilters.filterTag(data, "Fighter")).toEqual(fighters);
+  })
+  it('filtrar categoria Mage', () => {
+    const mages = [
+      {"name": "Ahri", "key": "103", "info": {"difficulty": "5"}, "tags": ["Mage", "Assassin"]}
+   ]
+     expect(allFilters.filterTag(data, "Mage")).toEqual(mages);
+  })
+  it('filtrar categoria Assassin', () => {
+    const assassins = [
+      {"name": "Zed", "key": "238", "info": {"difficulty": "7"}, "tags": ["Assassin", "Fighter"]},
+      {"name": "Ahri", "key": "103", "info": {"difficulty": "5"}, "tags": ["Mage", "Assassin"]}
+  ]
+    expect(allFilters.filterTag(data, "Assassin")).toEqual(assassins);
+  })
+  it('filtrar categoria Support', () => {
+    const supports = [
+      {"name": "Ashe", "key": "22", "info": {"difficulty": "4"}, "tags": ["Marksman", "Support"]}
+  ]
+    expect(allFilters.filterTag(data, "Support")).toEqual(supports);
+  })
+  it('filtrar categoria Marksman', () => {
+    const marksmans = [
+      {"name": "Ashe", "key": "22", "info": {"difficulty": "4"}, "tags": ["Marksman", "Support"]}
+  ]
+    expect(allFilters.filterTag(data, "Marksman")).toEqual(marksmans);
+  })
  });
 
- describe('filterDifficultyEasy', () => {
+ const dificuldadePersonagens = [
+  {"name": "Aatrox", "key": "266", "info": {"difficulty": "4"}, "tags": ["Fighter", "Tank"]},
+  {"name": "Ahri", "key": "103", "info": {"difficulty": "5"}, "tags": ["Mage", "Assassin"]},
+  {"name": "Anivia", "key": "34", "info": {"difficulty": "10"}, "tags": ["Mage", "Support"]},
+  {"name": "Zed", "key": "238", "info": {"difficulty": "7"}, "tags": ["Assassin", "Fighter"]}
+]
+  describe('filterDifficultyEasy', () => {
   it('should be a function', () => {
-    expect(typeof allFilters.filterTag).toBe('function');
+    expect(typeof allFilters.filterDifficultyEasy).toBe('function');
   })
-//   it('should return "" for ""', () => {
-//     expect(allFilters.filterDifficultyEasy()).toBe();
-//   })
- });
+  it('filtrar dificuldade fácil', () => {
+    const personagensFaceis = [
+      {"name": "Aatrox", "key": "266", "info": {"difficulty": "4"}, "tags": ["Fighter", "Tank"]},
+      {"name": "Ahri", "key": "103", "info": {"difficulty": "5"}, "tags": ["Mage", "Assassin"]}
+    ]
+    expect(allFilters.filterDifficultyEasy(dificuldadePersonagens, 5)).toEqual(personagensFaceis)
+ })
+});
 
- describe('filterDifficultyHard', () => {
-  it('should be a function', () => {
-    expect(typeof allFilters.filterTag).toBe('function');
-  })
-//   it('should return "" for ""', () => {
-//     expect(allFilters.filterDifficultyHard()).toBe();
-//   })
- });
+  describe('filterDifficultyHard', () => {
+    it('should be a function', () => {
+      expect(typeof allFilters.filterDifficultyHard).toBe('function');
+    })
+    it('filtrar dificuldade dificil', () => {
+     const personagensDificeis = [
+      {"name": "Anivia", "key": "34", "info": {"difficulty": "10"}, "tags": ["Mage", "Support"]},
+      {"name": "Zed", "key": "238", "info": {"difficulty": "7"}, "tags": ["Assassin", "Fighter"]}
+    ]
+      expect(allFilters.filterDifficultyHard(dificuldadePersonagens, 6)).toEqual(personagensDificeis)
+    })
+  });
